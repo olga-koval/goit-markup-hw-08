f6776b622fedc8dea7b665288be84a4e9087c219
Olga Koval <weddingkoval@gmail.com>
2021-12-22 23:49:46 +0200

    updated menu js

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,14 +1,3 @@
-// (() => {
-//   const refs = {
-//     openMenuBtn: document.querySelector('.menu-open-btn'),
-//     closeMenuBtn: document.querySelector('.menu-close-btn'),
-//     menu: document.querySelector('.mob-menu'),
-//   };  refs.openMenuBtn.addEventListener('click', toggleMenu);
-//   refs.closeMenuBtn.addEventListener('click', toggleMenu);  function toggleMenu() {
-//     refs.menu.classList.toggle('is-hidden');
-//   }
-// })();
-
 (() => {
   const menuBtnRef = document.querySelector('[data-menu-button]');
   const mobileMenuRef = document.querySelector('[data-menu]');
